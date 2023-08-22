8a2a292b6388b0c93cfd5ede6e7a22c12dba479e
Zouthen <mich5n69@stud.kea.dk>
2023-08-22 11:49:30 +0200

    03.Objects.js done

--- 00_Miscellaneous/01_Intro_Excercises/03.Objects.js
+++ 00_Miscellaneous/01_Intro_Excercises/03.Objects.js
@@ -6,20 +6,24 @@
 const myObj = { message: "Hello, earthling! I bring peace." };
 
 // Log the message 
+console.log(myObj.message)
 
 // --------------------------------------
 // Exercise 2 - Defining an object. 
 
 // Create an object that has your name and age. 
-
+const personObj = { name: "Michael", age: 26 };
+console.log(personObj)
 
 // --------------------------------------
 // Exercise 3 - Add a property 
 
 const stackOverflow = {};
+stackOverflow.add = "name";
 
 // make a rule called isAllowed and let the value be true
-
+stackOverflow.isAllowed = true;
+console.log(stackOverflow.isAllowed)
 // --------------------------------------
 // Exercise 4 - Remove a property 
 
@@ -27,5 +31,11 @@ const thisSong = { description: "The best song in the world." };
 
 // remove the property "description" and add a property called "about" that should say "Just a tribute." 
 
+console.log(thisSong)
+delete thisSong.description;
+console.log(thisSong)
+
+thisSong.about = "Just a tribute."
+console.log(thisSong)
 
 // --------------------------------------
