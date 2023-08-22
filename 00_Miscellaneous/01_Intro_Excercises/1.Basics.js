144bcc59b0a30e7b643d0a7fa9bf6752f4624afc
Zouthen <mich5n69@stud.kea.dk>
2023-08-22 10:46:30 +0200

    Solved 1.Basics from 01_Intro_Excercises

---
+++ 00_Miscellaneous/01_Intro_Excercises/1.Basics.js
@@ -0,0 +1,29 @@
+// --------------------------------------
+// Variables, strings, numbers, floats
+// --------------------------------------
+// Exercise 1 - Console and constiables
+
+const firstName = "Anders";
+const lastName = "Latif";
+// EXERCISE
+// show in the console
+// My first name is Anders and my last name is Latif
+console.log("My first name is", firstName, "and my last name is", lastName);
+
+
+// --------------------------------------
+// Exercise 2 - Numbers and Strings
+
+const year = "2022";
+const number = 1;
+
+// Add the year plus the number
+// The result should be 2023
+// You cannot touch line 1 or 2
+console.log(parseInt(year) + number);
+console.log(Number(year) + number);
+console.log(+year + number);
+// --------------------------------------
+
+console.log(Number("3523aa123"))
+console.log(parseInt("3523aa123"))
