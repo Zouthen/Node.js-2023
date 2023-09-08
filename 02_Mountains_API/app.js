681f531a59ba9d36de690ce123b24bdd8958cc17
Zouthen <mich5n69@stud.kea.dk>
2023-09-08 12:24:00 +0200

    mountains api with CRUD

--- 02_Mountains_API/app.js
+++ 02_Mountains_API/app.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const app = express();
+app.use(express.json());
+
 
 const mountains = [
     { id: 1, name: "Mount Fuji", height: "3776" },
@@ -34,7 +36,61 @@ app.get("/mountains/:id", (req, res) => {
 });
 
 
+// Create a new mountain
+app.post("/mountains", (req, res) => {
+    const newMountain = req.body;
+    if (!newMountain.name || !newMountain.height) {
+        res.status(400).send({ error: "Name and height are required for creating a mountain" });
+    } else {
+        newMountain.id = mountains.length + 1;
+        mountains.push(newMountain);
+        res.status(201).send({ data: newMountain });
+    }
+});
 
+
+// Delete a mountain by id
+app.delete("/mountains/:id", (req, res) => {
+    const pathVariableMountainId = parseInt(req.params.id);
+    if (isNaN(pathVariableMountainId)) {
+        res.status(400).send({ error: "The mountain id must be a number" });
+    } else {
+        const index = mountains.findIndex((mountain) => mountain.id === pathVariableMountainId);
+        if (index !== -1) {
+            const deletedMountain = mountains.splice(index, 1)[0];
+            res.send({ data: deletedMountain });
+        } else {
+            res.status(404).send({ error: "Mountain not found" });
+        }
+    }
+});
+
+// Update a mountain by id
+app.patch("/mountains/:id", (req, res) => {
+    const pathVariableMountainId = parseInt(req.params.id);
+    if (isNaN(pathVariableMountainId)) {
+        res.status(400).send({ error: "The mountain id must be a number" });
+    } else {
+        const updatedMountain = req.body;
+        if (!updatedMountain.name && !updatedMountain.height) {
+            res.status(400).send({ error: "At least one of 'name' or 'height' must be provided for updating a mountain" });
+        } else {
+            const index = mountains.findIndex((mountain) => mountain.id === pathVariableMountainId);
+            if (index !== -1) {
+                // Partially update the mountain with the provided data
+                if (updatedMountain.name) {
+                    mountains[index].name = updatedMountain.name;
+                }
+                if (updatedMountain.height) {
+                    mountains[index].height = updatedMountain.height;
+                }
+                res.send({ data: mountains[index] });
+            } else {
+                res.status(404).send({ error: "Mountain not found" });
+            }
+        }
+    }
+});
 
 const PORT = 8080;
 app.listen(PORT, (error) => {
