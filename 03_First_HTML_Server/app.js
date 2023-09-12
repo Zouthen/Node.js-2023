610acd2b27105b885dbc893892db46f7c4c66eca
Zouthen <mich5n69@stud.kea.dk>
2023-09-12 11:45:52 +0200

    03_First_HTML_Server update

--- 03_First_HTML_Server/app.js
+++ 03_First_HTML_Server/app.js
@@ -12,8 +12,18 @@ app.get("/secondPage", (req, res) => {
     res.sendFile(__dirname + "/public/secondPage.html");
 });
 
+// ===================================================
 
+app.get("/welcomeMessage", (req, res) => {
+    const clientName = req.query.user;
+    if (!clientName) {
+        res.send({ data: "Hello stranger"})
+    } else {
+        res.send({data: `Welcome to my fancy website, ${clientName}`});
+    }
+});
 
+// ===================================================
 const PORT = 8080;
 app.listen(PORT, (error) => {
     if (error) {
