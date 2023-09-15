289fbf2b90b544c2a15318036c6fa6cf03cdcaf2
Zouthen <mich5n69@stud.kea.dk>
2023-09-15 12:55:11 +0200

    update

--- TimeToDeploy/public/script.js
+++ TimeToDeploy/public/script.js
@@ -1,11 +1,1 @@
-document.addEventListener('DOMContentLoaded', () => {
-  const currentTimeElement = document.getElementById('currentTime');
 
-  function updateCurrentTime() {
-    const currentTime = new Date().toLocaleTimeString();
-    currentTimeElement.textContent = `Current Time: ${currentTime}`;
-  }
-
-  updateCurrentTime();
-  setInterval(updateCurrentTime, 1000);
-});
