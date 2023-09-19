f76c6871c3008629aa62f9061e0885049ca9abd3
Zouthen <mich5n69@stud.kea.dk>
2023-09-19 10:49:16 +0200

    05_Pokemon start

---
+++ 05_Pokemon/app.js
@@ -0,0 +1,20 @@
+import express from "express";
+const app = express();
+
+import path from "path";
+
+
+app.get("/", (req, res) => {
+    res.sendFile(path.resolve("./public/frontpage.html"));
+});
+
+    
+
+const PORT = 8080;
+app.listen(PORT, (error) => {
+    if (error){
+        console.log("Error starting the server", error);
+        return;
+    }
+    console.log("Server is running on port",8080);
+});
