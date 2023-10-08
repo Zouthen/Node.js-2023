421e6f5059b2a9d81eed985d5b2a041c306802b9
Zouthen <mich5n69@stud.kea.dk>
2023-10-08 12:14:30 +0200

    removed old unused parts

--- Mandatory_01-text/public/frontpage.js
+++
@@ -1,7 +0,0 @@
-// add them user name to the text
-fetch("/welcomeMessage?user=Michael")
-    .then((response) => response.json())
-    .then((result) => {
-        const welcomeMessageHeader = document.getElementById("welcomeMessage")
-        welcomeMessageHeader.innerText = result.data;
-    });    
