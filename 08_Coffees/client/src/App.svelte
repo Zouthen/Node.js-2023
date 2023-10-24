d989d055288892cb0fd28bd7a71872a75c13fefd
Zouthen <mich5n69@stud.kea.dk>
2023-10-24 09:56:58 +0200

    08_coffees

---
+++ 08_Coffees/client/src/App.svelte
@@ -0,0 +1,24 @@
+<script>
+    import { Router, Link, Route } from "svelte-navigator";
+    
+    import Home from "./pages/Home/Home.svelte";
+    import CoffeeFatcs from "./pages/CoffeeFacts/CoffeeFatcs.svelte";
+    import CoffeeOverview from "./pages/CoffeeOverview/CoffeeOverview.svelte";
+
+</script>
+
+<Router>
+    <nav>
+        <Link to="/">Home</Link>
+        <Link to="/coffeetypes">Coffe Overview</Link>
+        <Link to="/coffeefacts">Coffee Facts</Link>
+    </nav>
+
+    <div>
+        <Route path="/" component={Home}></Route>
+        <Route path="/coffeetypes"><CoffeeOverview/></Route>
+        <Route path="/coffeefacts"><CoffeeFatcs/></Route>
+    </div>
+
+</Router>
+    
---
+++ 08_Coffees/client/src/pages/CoffeeOverview/CoffeeOverview.svelte
@@ -0,0 +1,1 @@
+<h1>Coffee Selection...</h1>
