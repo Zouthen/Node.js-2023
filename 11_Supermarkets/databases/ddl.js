083f18cffb13991e18ddb8c09d401ad71a82180a
Zouthen <mich5n69@stud.kea.dk>
2023-11-07 10:40:29 +0100

    start on supermarkets db

---
+++ 11_Supermarkets/databases/ddl.js
@@ -0,0 +1,15 @@
+
+
+// Task Write SQL to create a supermarkets table, you decide on the design.
+`
+
+CREATE TABLE supermarkets (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    name VARCHAR(50),
+    location VarChar(100),
+    rating INTEGER
+);
+
+
+
+`
